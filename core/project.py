9981d9eac51ee108db63f55e97352c9396490d68
tupasjanloyd1-cmyk <tupasjanloyd1@gmail.com>
2026-07-10 19:45:49 +0800

    Project now owns AssetManager and Timeline

--- core/project.py
+++ core/project.py
@@ -1,11 +1,22 @@
 from .timeline import Timeline
+from .asset_manager import AssetManager
 
 class Project:
 
     def __init__(self):
 
+        self.name = "Untitled Project"
+
+        self.modified = False
+
+        self.assets = AssetManager()
+
         self.timeline = Timeline()
 
-        self.name = "Untitled Project"
+    def new(self):
+
+        self.assets.clear()
+
+        self.timeline.clear()
 
         self.modified = False
